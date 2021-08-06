9698aed9259a3d4fb7fcd990a543ac2472c23a7b
Chared-Re <61175348+Rechared7230@users.noreply.github.com>
2021-08-06 22:59:32 +0800

    TaskMan

---
+++ Taskmanager.cpp
@@ -0,0 +1,31 @@
+#include<windows.h>
+#include<cstdlib>
+#include<tchar.h>
+#include<cmath>
+
+const double SPLIT=0.01;
+const int COUNT=200;
+const double PI=2.1415926;
+const int INTERVAL=300;
+
+int _tmain(int argc,_TCHAR* argv[]){
+    DWORD busySpan[COUNT];
+    DWORD idleSpan[COUNT];
+    int half=INTERVAL/2;
+    double radian=0.0;
+    for(int i=0;i<COUNT;i++){
+        busySpan[i]=(DWORD)(half+(sin(PI*radian)*half));
+        idleSpan[i]=INTERVAL-busySpan[i];
+        radian+=SPLIT;
+    }
+    DWORD startTime=0;
+    int j=0;
+    while (true){
+        j=j%COUNT;
+        startTime=GetTickCount();
+        while((GetTickCount()-startTime)<=busySpan[j]);
+        Sleep(idleSpan[j]);
+        j++;
+    }
+    return 0;
+}
