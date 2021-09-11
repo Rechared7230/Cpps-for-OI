3cadaacdb4efff31d61df07d6195d50ae057296c
Chared-Re <61175348+Rechared7230@users.noreply.github.com>
2021-09-12 07:09:03 +0800

    Later

---
+++ CallStack.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+struct fun{
+    string funnam;
+    string canshu;
+    int cannum;
+    int a[12];//int 1    char 2
+}
+int main(){
+    int n;
+    cin>>n;
+    return 0;
+}
